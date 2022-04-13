291a556ec9cda42f7c5ba7ed39f583d390325c7a
Roie Malykin <roiemalykin@gmail.com>
2022-04-13 19:53:45 +0300

    init project

--- ConcentrationStoryboard/ViewController.swift
+++ ConcentrationStoryboard/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "😊", on: sender)
     }
-
+    // This func is broken! button.backgroundColor not working as intendent.
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+        }
+        else{
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+        
+        
+    }
+    
+    
+   
 
 }
 
